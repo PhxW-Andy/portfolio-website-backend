c328e4dd6db524d5781570efdee6de3d225667b7
Andre Lebioda <50991147+PhxW-Andy@users.noreply.github.com>
2022-08-20 01:07:51 +0200

    changes

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ app.post("/send_mail", cors(), async (req, res) => {
 });
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running `);
 });
