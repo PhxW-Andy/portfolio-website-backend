81d989c76bcd0565b803815dc03f005441ab3653
Andre Lebioda <50991147+PhxW-Andy@users.noreply.github.com>
2022-08-20 01:48:27 +0200

    changesh

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const nodemailer = require("nodemailer");
 app.use(express.json());
 app.use(cors());
 
-app.post("/", cors(), async (req, res) => {
+app.get("/", (req, res) => {
+  res.send("is running");
+});
+
+app.post("/send_mail", cors(), async (req, res) => {
   // create reusable transporter object using the default SMTP transport
 
   const { name, email, message } = req.body;
