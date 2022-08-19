8d0ca10bb93458e78748d464d8bb5589635df878
Andre Lebioda <50991147+PhxW-Andy@users.noreply.github.com>
2022-08-20 01:02:26 +0200

    changes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const PORT = 5000;
 const cors = require("cors");
 
 const nodemailer = require("nodemailer");
@@ -44,6 +43,6 @@ app.post("/send_mail", cors(), async (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
